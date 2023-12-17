2d673c700d3eb30c39b481abf4581f118dba31f8
john potter <juanpotter7@gmail.com>
2023-12-17 02:01:17 -0500

    post to adb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from dotenv import dotenv_values
+import json
 config = dotenv_values(".env")
 authToken = HTTPBasicAuth(config['CB_USER'],config['CB_PASS'])
 baseUrl = 'http://35.196.111.98:8093/query/service?statement='
@@ -11,4 +12,5 @@ resultObj = r.json()['results']
 releaseService = resultObj[0]['release-service']['data']
 
 for audit in releaseService:
-    print(audit)
+    json_object = json.dumps(audit, indent = 4) 
+    requests.post('http://34.73.174.64:8529/_db/_system/foxx/audit', data=json_object)
